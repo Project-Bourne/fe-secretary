159c8e8cb783c167f013f1bf594d03267081538e
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-10-11 12:34:26 +0100

    fix errror

--- src/components/layouts/Header/DropdownItems.tsx
+++ src/components/layouts/Header/DropdownItems.tsx
@@ -1,69 +1,60 @@
-import Link from "next/link";
-import { useSelector } from "react-redux";
-import interrogator from "../../../../public/icons/interrogator.svg";
-import summarizer from "../../../../public/icons/summarizer.svg";
-import irp from "../../../../public/icons/irp.svg";
-import translator from "../../../../public/icons/translator.svg";
-import collab from "../../../../public/icons/collab.svg";
-import admin from "../../../../public/icons/admin.svg";
-import fact_checker from "../../../../public/icons/checker.svg";
-import analyzer from "../../../../public/icons/analyzer.svg";
-import deep_chat from "../../../../public/icons/deep.svg";
-import Image from "next/image";
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import interrogator from '../../../../public/icons/interrogator.svg';
+import irp from '../../../../public/icons/irp.svg';
+import translator from '../../../../public/icons/translator.svg';
+import collab from '../../../../public/icons/collab.svg';
+import fact from "../../../../public/icons/fact.svg";
+import analyzer from '../../../../public/icons/analyzer.svg';
+import deep_chat from '../../../../public/icons/deep.svg';
+import Image from 'next/image';
 
-const BASE_URL = "http://192.81.213.226";
+const BASE_URL = 'http://192.81.213.226';
 
 const dropdownItems = [
-  // {
-  //   name: "Admin",
-  //   to: `${BASE_URL}:30/home`,
-  //   key: "admin",
-  //   icon: admin,
-  // },
   {
-    name: "IRP",
-    to: `${BASE_URL}:30/home`,
-    key: "irp",
-    icon: irp,
+    name: 'IRP',
+    to: `${BASE_URL}/30/home`,
+    key: 'irp',
+    icon: irp
   },
   {
-    name: "Collab",
-    to: `${BASE_URL}:36/home`,
-    key: "collab",
-    icon: collab,
+    name: 'Collab',
+    to: `${BASE_URL}/36/home`,
+    key: 'collab',
+    icon: collab
   },
   {
-    name: "Analyzer",
-    to: `${BASE_URL}:31/home`,
-    key: "analyser",
-    icon: analyzer,
+    name: 'Analyzer',
+    to: `${BASE_URL}/31/home`,
+    key: 'analyser',
+    icon: analyzer
   },
   {
-    name: "Fact Checker",
-    to: `${BASE_URL}:34/home`,
-    key: "fact checker",
-    icon: fact_checker,
+    name: 'Fact checker',
+    to: `${BASE_URL}/34/home`,
+    key: 'fact checker',
+    icon: fact
   },
   {
-    name: "Translator",
-    to: `${BASE_URL}:33/home`,
-    key: "translator",
-    icon: translator,
+    name: 'Translator',
+    to: `${BASE_URL}/33/home`,
+    key: 'translator',
+    icon: translator
   },
-
   {
-    name: "Deep Chat",
-    to: `${BASE_URL}:35/home`,
-    key: "deep chat",
-    icon: deep_chat,
+    name: 'Deep Chat',
+    to: `${BASE_URL}/35/home`,
+    key: 'deep chat',
+    icon: deep_chat
   },
 
   {
-    name: "Interrogator",
-    to: `${BASE_URL}:37/home`, // change route
-    key: "interrogator",
-    icon: interrogator,
-  },
+    name: 'Interrogator',
+    to: `${BASE_URL}/37/home`, // change route
+    key: 'interrogator',
+    icon: interrogator
+  }
 ];
 
 function DashboardDropdown() {
@@ -73,35 +64,35 @@ function DashboardDropdown() {
   );
 
   return (
-    <ul className="bg-sirp- shadow absolute top-[3rem] rounded z-30 w-[130px]">
+    <ul className="bg-sirp-lightGrey shadow absolute top-[3rem] pt-1 flex md:grid grid-cols-3 rounded z-30 w-[130px] md:w-[300px]">
       {dropdownItems.map((item, index) => {
-        const shouldRender =
-          item.key === "irp" || permissions.includes(item.key);
-        if (shouldRender) {
-          return (
+        const shouldRender = item.key === 'irp' || permissions.includes(item.key);
+        
+        return (
+          shouldRender && (
             <li
               key={index}
-              className="py-1.5 px-2 bg-sirp-lightGrey text-black border-b-[1px] border-b-gray-200/[0.5] text-[12px]"
+              className="py-1.5 px-2  text-black border-b-[1px] border-r-[1px] border-b-gray-200/[0.5] border-r-gray-200/[0.5] text-[12px] text-center"
             >
-              <Link href={item.to} className="flex gap-x-3 items-center">
+              <Link href={item.to} className="grid gap-x-3 items-center">
                 <Image
                   src={item.icon}
                   alt={item.key}
                   className={`${
                     item.key !== "deep chat"
-                      ? "h-[10px] w-[10px]"
-                      : "h-[20px] w-[10px]"
+                      ? "h-[10px] w-[10px] md:mx-auto"
+                      : "h-[20px] w-[10px] md:mx-auto"
                   } `}
                 />
                 <span>{item.name}</span>
               </Link>
             </li>
-          );
-        }
-        return null; // Do not render this item if the condition is not met
+          )
+        );
       })}
     </ul>
   );
 }
 
 export default DashboardDropdown;
+
